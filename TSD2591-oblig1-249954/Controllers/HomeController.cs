f3263c86fe91bebfc517baf4b8c8c3964ceb485f
Simen Sommerud <jobbsimen13@gmail.com>
2025-04-25 12:08:04 +0200

    Modul/ Commit1
    Oppsett av prosjekt i Visual Studio Code (oppretting av arbeids-katalog og solution-fil (.snl)

---
+++ TSD2591-oblig1-249954/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TSD2591_oblig1_249954.Models;
+
+namespace TSD2591_oblig1_249954.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ TSD2591-oblig1-249954/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2591_oblig1_249954.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
