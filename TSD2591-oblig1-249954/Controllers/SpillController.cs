9c1fafda23cc2d903a0cae83109e505fb20fa544
Simen Sommerud <jobbsimen13@gmail.com>
2025-04-25 21:02:18 +0200

    Modul/ Commit4c
    spill har brukere med oppdaterbar antall spill spilt

--- TSD2591-oblig1-249954/Controllers/SpillController.cs
+++ TSD2591-oblig1-249954/Controllers/SpillController.cs
@@ -1,10 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using TSD2591_oblig1_249954.Data;   
+using TSD2591_oblig1_249954.Models;  
+using System.Linq;
 
-public class SpillController : Controller
+namespace TSD2591_oblig1_249954.Controllers
 {
-    public IActionResult Spill2Like()
+    public class SpillController : Controller
     {
-        return View();
+        private readonly ApplicationDbContext _context;
+
+        // Constructor for å hente inn DbContext
+        public SpillController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Viser spillet
+        public IActionResult Spill2Like()
+        {
+            return View();
+        }
+
+        // Tar imot POST fra fetch når spillet er ferdig og oppdaterer antall spill for bruker
+        [HttpPost]
+        public IActionResult OppdaterSpillCount([FromBody] BrukerData data)
+        {
+            var bruker = _context.Brukere.FirstOrDefault(b => b.Navn == data.Navn);
+            if (bruker != null)
+            {
+                bruker.AntallSpill += 1;
+                _context.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+
+    // Klasse for å ta imot JSON-data fra fetch (navn på bruker)
+    public class BrukerData
+    {
+        public string Navn { get; set; }
     }
 }
-
